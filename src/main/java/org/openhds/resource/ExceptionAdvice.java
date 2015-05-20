80039d2d851e5bbb21057c17f59284db70a8c7d7
Ben Heasly <benjamin.heasly@gmail.com>
2015-05-20 14:33:42 -0400

    Add Exception advice for 404 on bad query

--- src/main/java/org/openhds/resource/ExceptionAdvice.java
+++ src/main/java/org/openhds/resource/ExceptionAdvice.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.NoSuchElementException;
+
 /**
  * Created by Ben on 5/18/15.
  *
@@ -15,19 +17,26 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  *
  */
 @ControllerAdvice
-class ExceptionControllerAdvice {
+class ExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public VndErrors generalException(Exception ex) {
-        return new VndErrors("general error: ", ex.getMessage());
+        return new VndErrors(ex.getClass().getSimpleName() + ": ", ex.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public VndErrors notFoundException(NoSuchElementException ex) {
+        return new VndErrors("Nothing found: ", ex.getMessage());
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public VndErrors userNotFoundExceptionHandler(UsernameNotFoundException ex) {
-        return new VndErrors("user not found: ", ex.getMessage());
+        return new VndErrors("User not found: ", ex.getMessage());
     }
 }
