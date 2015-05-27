bb548b66f3b89ff9aee207f1bdf38243ae7ad6c5
Ben Heasly <benjamin.heasly@gmail.com>
2015-05-27 17:21:15 -0400

    remove EntityControllerRegistry in favor of Spring Hateoas EntityLinks and annotation scanning

--- src/main/java/org/openhds/resource/controller/UserRestController.java
+++ src/main/java/org/openhds/resource/controller/UserRestController.java
@@ -1,11 +1,12 @@
 package org.openhds.resource.controller;
 
 import org.openhds.repository.UserRepository;
-import org.openhds.resource.EntityControllerRegistry;
+import org.openhds.resource.ResourceLinkAssembler;
 import org.openhds.security.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +21,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  */
 @RestController
 @RequestMapping("/users")
+@ExposesResourceFor(User.class)
 class UserRestController extends AbstractRestController<User> {
 
     private final UserRepository userRepository;
 
     @Autowired
-    public UserRestController(EntityControllerRegistry entityControllerRegistry, UserRepository userRepository) {
-        super(User.class, entityControllerRegistry);
+    public UserRestController(ResourceLinkAssembler resourceLinkAssembler, UserRepository userRepository) {
+        super(resourceLinkAssembler);
         this.userRepository = userRepository;
     }
 
