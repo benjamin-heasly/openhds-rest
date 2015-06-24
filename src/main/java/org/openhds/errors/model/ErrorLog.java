b671977341c9b62d2a34f3dc4de37b72641c93d9
Ben Heasly <benjamin.heasly@gmail.com>
2015-06-24 11:04:25 -0400

    bring in Error, ErrorLog, and utils from openhds-server

---
+++ src/main/java/org/openhds/errors/model/ErrorLog.java
@@ -0,0 +1,81 @@
+package org.openhds.errors.model;
+
+import org.openhds.domain.contract.AuditableCollectedEntity;
+import org.openhds.domain.util.Description;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+
+@Description(description = "A log entry containing multiple errors.")
+@Entity
+@Table(name = "errorlog")
+public class ErrorLog extends AuditableCollectedEntity implements Serializable {
+
+    private static final long serialVersionUID = 2447734552586256198L;
+
+    @Column(length=65535)
+    private String dataPayload;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, targetEntity = Error.class)
+    @JoinColumn(name = "error_uuid")
+    private List<Error> errors;
+
+    private String assignedTo;
+
+    @Column
+    private String entityType;
+
+    private String resolutionStatus;
+
+    private Calendar dateOfResolution;
+
+    public String getDataPayload() {
+        return dataPayload;
+    }
+
+    public void setDataPayload(String dataPayload) {
+        this.dataPayload = dataPayload;
+    }
+
+    public List<Error> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<Error> errors) {
+        this.errors = errors;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(String entityType) {
+        this.entityType = entityType;
+    }
+
+    public String getResolutionStatus() {
+        return resolutionStatus;
+    }
+
+    public void setResolutionStatus(String resolutionStatus) {
+        this.resolutionStatus = resolutionStatus;
+    }
+
+    public Calendar getDateOfResolution() {
+        return dateOfResolution;
+    }
+
+    public void setDateOfResolution(Calendar dateOfResolution) {
+        this.dateOfResolution = dateOfResolution;
+    }
+}
---
+++ src/main/java/org/openhds/errors/util/ErrorConstants.java
@@ -0,0 +1,13 @@
+package org.openhds.errors.util;
+
+public class ErrorConstants {
+
+    public static final String RESOLVED_ERROR_STATUS = "Resolved";
+    public static final String UNRESOLVED_ERROR_STATUS = "Unresolved";
+    public static final String UNASSIGNED = "Unassigned";
+    public static final String CONSTRAINT_VIOLATION = "Constraint Violation";
+
+    public static final String MODIFIED_EXTID = "Modified ExtId";
+    public static final String DUPLICATE_EXTID = "Duplicate ExtId";
+
+}
