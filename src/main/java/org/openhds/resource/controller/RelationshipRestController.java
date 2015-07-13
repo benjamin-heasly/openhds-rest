91d081e9ccc0558d35c7e9f593f348fb5be7383d
Wolfe <nathan.lienhardt@maine.edu>
2015-07-13 17:48:15 -0400

    Added the relationship rest controller and tests with registration as well

--- src/main/java/org/openhds/resource/controller/RelationshipRestController.java
+++ src/main/java/org/openhds/resource/controller/RelationshipRestController.java
@@ -1,7 +1,57 @@
 package org.openhds.resource.controller;
 
+import org.openhds.domain.model.Individual;
+import org.openhds.domain.model.Relationship;
+import org.openhds.resource.contract.AuditableCollectedRestController;
+import org.openhds.resource.registration.RelationshipRegistration;
+import org.openhds.service.impl.FieldWorkerService;
+import org.openhds.service.impl.IndividualService;
+import org.openhds.service.impl.RelationshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Wolfe on 7/13/2015.
  */
-public class RelationshipRestController {
+@RestController
+@RequestMapping("/relationships")
+@ExposesResourceFor(Relationship.class)
+public class RelationshipRestController extends AuditableCollectedRestController<
+        Relationship,
+        RelationshipRegistration,
+        RelationshipService> {
+
+    private RelationshipService relationshipService;
+
+    private IndividualService individualService;
+
+    private FieldWorkerService fieldWorkerService;
+
+
+    @Autowired
+    public RelationshipRestController(RelationshipService relationshipService,
+                                      IndividualService individualService,
+                                      FieldWorkerService fieldWorkerService) {
+        super(relationshipService);
+        this.relationshipService = relationshipService;
+        this.individualService = individualService;
+        this.fieldWorkerService = fieldWorkerService;
+    }
+
+    @Override
+    protected Relationship register(RelationshipRegistration registration) {
+        Relationship relationship = registration.getRelationship();
+        relationship.setIndividualA(individualService.findOne(registration.getIndividualAId()));
+        relationship.setIndividualB(individualService.findOne(registration.getIndividualBId()));
+        relationship.setCollectedBy(fieldWorkerService.findOne(registration.getCollectedByUuid()));
+        return relationshipService.createOrUpdate(relationship);
+    }
+
+    @Override
+    protected Relationship register(RelationshipRegistration registration, String id) {
+        registration.getRelationship().setUuid(id);
+        return register(registration);
+    }
 }
--- src/main/java/org/openhds/resource/registration/RelationshipRegistration.java
+++ src/main/java/org/openhds/resource/registration/RelationshipRegistration.java
@@ -1,7 +1,39 @@
 package org.openhds.resource.registration;
 
+import org.openhds.domain.model.Relationship;
+
 /**
  * Created by Wolfe on 7/13/2015.
  */
-public class RelationshipRegistration {
+public class RelationshipRegistration extends Registration<Relationship> {
+
+    private Relationship relationship;
+
+    private String individualAId;
+
+    private String individualBId;
+
+    public Relationship getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(Relationship relationship) {
+        this.relationship = relationship;
+    }
+
+    public String getIndividualAId() {
+        return individualAId;
+    }
+
+    public void setIndividualAId(String individualAId) {
+        this.individualAId = individualAId;
+    }
+
+    public String getIndividualBId() {
+        return individualBId;
+    }
+
+    public void setIndividualBId(String individualBId) {
+        this.individualBId = individualBId;
+    }
 }
