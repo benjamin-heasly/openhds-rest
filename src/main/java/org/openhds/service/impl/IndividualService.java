1685785bfc196c83157eb02b2e70d7a9caacc2bd
Wolfe <nathan.lienhardt@maine.edu>
2015-07-13 14:55:29 -0400

    Added individual service and service tests

--- src/main/java/org/openhds/service/impl/IndividualService.java
+++ src/main/java/org/openhds/service/impl/IndividualService.java
@@ -1,7 +1,34 @@
 package org.openhds.service.impl;
 
+import org.openhds.domain.model.Individual;
+import org.openhds.repository.concrete.IndividualRepository;
+import org.openhds.service.contract.AbstractAuditableExtIdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+
 /**
  * Created by Wolfe on 7/13/2015.
  */
-public class IndividualService {
+@Service
+public class IndividualService extends AbstractAuditableExtIdService<Individual, IndividualRepository>{
+
+    @Autowired
+    private FieldWorkerService fieldWorkerService;
+
+    @Autowired
+    public IndividualService(IndividualRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected Individual makeUnknownEntity() {
+        Individual individual = new Individual();
+        individual.setFirstName("unknown");
+        individual.setExtId("unknown");
+        individual.setCollectedBy(fieldWorkerService.getUnknownEntity());
+        individual.setCollectionDateTime(ZonedDateTime.now());
+        return individual;
+    }
 }
