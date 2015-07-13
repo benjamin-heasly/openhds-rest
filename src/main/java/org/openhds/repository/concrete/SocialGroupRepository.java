9b21644d4d2b2de7c6755a9dcdd2f9b4e1b22434
Ben Heasly <benjamin.heasly@gmail.com>
2015-07-13 15:31:35 -0400

    Social Group repository

---
+++ src/main/java/org/openhds/repository/concrete/SocialGroupRepository.java
@@ -0,0 +1,11 @@
+package org.openhds.repository.concrete;
+
+        import org.openhds.domain.model.SocialGroup;
+        import org.openhds.repository.contract.AuditableCollectedRepository;
+
+/**
+ * Created by bsh on 7/13/15.
+ */
+public interface SocialGroupRepository extends AuditableCollectedRepository<SocialGroup> {
+
+}
