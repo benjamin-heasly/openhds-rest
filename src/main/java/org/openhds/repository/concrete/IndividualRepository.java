a96ce52f764e57f48ab050ff5dc48830d5e3d51b
Wolfe <nathan.lienhardt@maine.edu>
2015-07-13 14:54:36 -0400

    Added individual Repository.

--- src/main/java/org/openhds/repository/concrete/IndividualRepository.java
+++ src/main/java/org/openhds/repository/concrete/IndividualRepository.java
@@ -1,7 +1,11 @@
 package org.openhds.repository.concrete;
 
+import org.openhds.domain.model.Individual;
+import org.openhds.repository.contract.AuditableExtIdRepository;
+
 /**
  * Created by Wolfe on 7/13/2015.
  */
-public interface IndividualRepository {
+public interface IndividualRepository extends AuditableExtIdRepository<Individual> {
+
 }
