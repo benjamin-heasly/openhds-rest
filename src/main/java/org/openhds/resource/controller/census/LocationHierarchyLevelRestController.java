d55c55ab8f914cce9abc1191680319d1135489b9
Ben Heasly <benjamin.heasly@gmail.com>
2015-07-20 11:44:40 -0400

    rest controllers for location hierarchy and level

---
+++ src/main/java/org/openhds/resource/controller/census/LocationHierarchyLevelRestController.java
@@ -0,0 +1,42 @@
+package org.openhds.resource.controller.census;
+
+import org.openhds.domain.model.census.LocationHierarchyLevel;
+import org.openhds.resource.contract.AuditableRestController;
+import org.openhds.resource.registration.census.LocationHierarchyLevelRegistration;
+import org.openhds.service.impl.census.LocationHierarchyLevelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Ben on 5/18/15.
+ */
+@RestController
+@RequestMapping("/locationHierarchyLevel")
+@ExposesResourceFor(LocationHierarchyLevel.class)
+public class LocationHierarchyLevelRestController extends AuditableRestController<
+        LocationHierarchyLevel,
+        LocationHierarchyLevelRegistration,
+        LocationHierarchyLevelService> {
+
+    private final LocationHierarchyLevelService locationHierarchyLevelService;
+
+    @Autowired
+    public LocationHierarchyLevelRestController(LocationHierarchyLevelService locationHierarchyLevelService) {
+        super(locationHierarchyLevelService);
+        this.locationHierarchyLevelService = locationHierarchyLevelService;
+    }
+
+    @Override
+    protected LocationHierarchyLevel register(LocationHierarchyLevelRegistration registration) {
+        LocationHierarchyLevel locationHierarchyLevel = registration.getLocationHierarchyLevel();
+        return locationHierarchyLevelService.createOrUpdate(locationHierarchyLevel);
+    }
+
+    @Override
+    protected LocationHierarchyLevel register(LocationHierarchyLevelRegistration registration, String id) {
+        registration.getLocationHierarchyLevel().setUuid(id);
+        return register(registration);
+    }
+}
