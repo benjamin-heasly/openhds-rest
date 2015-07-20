689549fb16b6f7f939c149491c1a45ec21ea9da8
Wolfe <nathan.lienhardt@maine.edu>
2015-07-20 10:13:13 -0400

    Added find or make placeholder method to uuid superservice

--- src/main/java/org/openhds/service/contract/AbstractUuidService.java
+++ src/main/java/org/openhds/service/contract/AbstractUuidService.java
@@ -96,6 +96,16 @@ public abstract class AbstractUuidService<T extends UuidIdentifiable, V extends 
         return repository.findOne(id);
     }
 
+    public T findOrMakePlaceHolder(String uuid){
+        T entity = findOne(uuid);
+        if (null == entity){
+            entity = getUnknownEntity();
+            entity.setUuid(uuid);
+            createOrUpdate(entity);
+        }
+        return entity;
+    }
+
     public T createOrUpdate(T entity) {
 
         ErrorLog errorLog = new ErrorLog();
