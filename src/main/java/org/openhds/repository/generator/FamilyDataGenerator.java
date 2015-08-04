fe48a38a2df6f74f667aed558893935140b1c09f
Ben Heasly <benjamin.heasly@gmail.com>
2015-08-04 13:26:20 -0400

    new family data generator

---
+++ src/main/java/org/openhds/repository/generator/FamilyDataGenerator.java
@@ -0,0 +1,224 @@
+package org.openhds.repository.generator;
+
+import org.openhds.domain.contract.AuditableCollectedEntity;
+import org.openhds.domain.contract.AuditableEntity;
+import org.openhds.domain.model.census.*;
+import org.openhds.repository.concrete.census.*;
+import org.openhds.security.model.User;
+import org.openhds.service.impl.FieldWorkerService;
+import org.openhds.service.impl.UserService;
+import org.openhds.service.impl.census.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Created by bsh on 4 August 2015.
+ * <p>
+ * Generates sample family data, including
+ * Individual, SocialGroup, Relationship, Residency, and Membership.
+ * <p>
+ * For each entity, only inserts sample records if there are no records yet.
+ * This behavior should support testing without messing up existing project data.
+ * <p>
+ * At each existing Location, creates a family of 10 Individuals.
+ * <p>
+ * Each family will be represented by:
+ * - a SocialGroup "household"
+ * - an Individual who is the "head of household"
+ * - nine individuals who are "members of the household"
+ * - 10 Residencies at the Location
+ * - 10 Memberships in the SocialGroup
+ * - 10 Relationships to the head of household
+ */
+@Component
+public class FamilyDataGenerator {
+
+    public static final int ARITY = 10;
+
+    private final IndividualRepository individualRepository;
+    private final IndividualService individualService;
+
+    private final SocialGroupRepository socialGroupRepository;
+    private final SocialGroupService socialGroupService;
+
+    private final ResidencyRepository residencyRepository;
+    private final ResidencyService residencyService;
+
+    private final MembershipRepository membershipRepository;
+    private final MembershipService membershipService;
+
+    private final RelationshipRepository relationshipRepository;
+    private final RelationshipService relationshipService;
+
+    private final LocationService locationService;
+
+    private final FieldWorkerService fieldWorkerService;
+
+    private final UserService userService;
+
+    @Autowired
+    public FamilyDataGenerator(IndividualRepository individualRepository,
+                               IndividualService individualService,
+                               SocialGroupRepository socialGroupRepository,
+                               SocialGroupService socialGroupService,
+                               ResidencyRepository residencyRepository,
+                               ResidencyService residencyService,
+                               MembershipRepository membershipRepository,
+                               MembershipService membershipService,
+                               RelationshipRepository relationshipRepository,
+                               RelationshipService relationshipService,
+                               LocationService locationService,
+                               FieldWorkerService fieldWorkerService,
+                               UserService userService) {
+        this.individualRepository = individualRepository;
+        this.individualService = individualService;
+        this.socialGroupRepository = socialGroupRepository;
+        this.socialGroupService = socialGroupService;
+        this.residencyRepository = residencyRepository;
+        this.residencyService = residencyService;
+        this.membershipRepository = membershipRepository;
+        this.membershipService = membershipService;
+        this.relationshipRepository = relationshipRepository;
+        this.relationshipService = relationshipService;
+        this.locationService = locationService;
+        this.fieldWorkerService = fieldWorkerService;
+        this.userService = userService;
+    }
+
+    public void generateData() {
+        generateUnknowns();
+        addFamilyToEach(locationService.findAll(new Sort("uuid")));
+    }
+
+    public void clearData() {
+        relationshipRepository.deleteAllInBatch();
+        membershipRepository.deleteAllInBatch();
+        residencyRepository.deleteAllInBatch();
+        individualRepository.deleteAllInBatch();
+        socialGroupRepository.deleteAllInBatch();
+    }
+
+    // trigger services to create unknown entities ahead of time, for predictable entity counts
+    private void generateUnknowns() {
+        individualService.getUnknownEntity();
+        socialGroupService.getUnknownEntity();
+        residencyService.getUnknownEntity();
+        relationshipService.getUnknownEntity();
+        membershipService.getUnknownEntity();
+    }
+
+    // add a family at each location
+    private void addFamilyToEach(Iterable<Location> locations) {
+        for (Location locaiton : locations) {
+            addFamily(locaiton);
+        }
+    }
+
+    // add a family at the given location
+    private void addFamily(Location location) {
+        SocialGroup socialGroup = generateSocialGroup(location.getExtId());
+
+        Individual head = generateIndividual(location.getName() + "-head");
+        generateRelationship(head, head, "self");
+        generateMembership(head, socialGroup, "self");
+        generateResidency(head, location);
+
+        // create arity-1 individuals
+        // create each relationship, membership, residency
+    }
+
+    private SocialGroup generateSocialGroup(String name) {
+        SocialGroup socialGroup = new SocialGroup();
+        setAuditableFields(socialGroup);
+        setCollectedFields(socialGroup);
+
+        socialGroup.setExtId(name);
+        socialGroup.setGroupName(name);
+        socialGroup.setGroupType("sample-household");
+
+        // save using repository, not service, for performance
+        return socialGroupRepository.save(socialGroup);
+    }
+
+    private Individual generateIndividual(String name) {
+        Individual individual = new Individual();
+        setAuditableFields(individual);
+        setCollectedFields(individual);
+
+        individual.setExtId(name);
+        individual.setFirstName(name);
+        individual.setLastName(name);
+
+        // save using repository, not service, for performance
+        return individualRepository.save(individual);
+    }
+
+    private void generateRelationship(Individual a, Individual b, String type) {
+        Relationship relationship = new Relationship();
+        setAuditableFields(relationship);
+        setCollectedFields(relationship);
+
+        relationship.setIndividualA(a);
+        relationship.setIndividualB(b);
+        relationship.setRelationshipType(type);
+        relationship.setStartDate(ZonedDateTime.now());
+
+        // save using repository, not service, for performance
+        relationshipRepository.save(relationship);
+    }
+
+    private void generateMembership(Individual individual, SocialGroup socialGroup, String relationToHead) {
+        Membership membership = new Membership();
+        setAuditableFields(membership);
+        setCollectedFields(membership);
+
+        membership.setIndividual(individual);
+        membership.setSocialGroup(socialGroup);
+        membership.setRelationshipToGroupHead(relationToHead);
+        membership.setStartDate(ZonedDateTime.now());
+        membership.setStartType("generated-sample");
+
+        // save using repository, not service, for performance
+        membershipRepository.save(membership);
+    }
+
+    private void generateResidency(Individual individual, Location location) {
+        Residency residency = new Residency();
+        setAuditableFields(residency);
+        setCollectedFields(residency);
+
+        residency.setIndividual(individual);
+        residency.setLocation(location);
+        residency.setStartDate(ZonedDateTime.now());
+        residency.setStartType("generated-sample");
+
+        // save using repository, not service, for performance
+        residencyRepository.save(residency);
+    }
+
+    private void setAuditableFields(AuditableEntity entity) {
+        User user = userService.getUnknownEntity();
+        ZonedDateTime now = ZonedDateTime.now();
+
+        //Check to see if we're creating or updating the entity
+        if (null == entity.getInsertDate()) {
+            entity.setInsertDate(now);
+        }
+
+        if (null == entity.getInsertBy()) {
+            entity.setInsertBy(user);
+        }
+
+        entity.setLastModifiedDate(now);
+        entity.setLastModifiedBy(user);
+    }
+
+    private void setCollectedFields(AuditableCollectedEntity entity) {
+        entity.setCollectedBy(fieldWorkerService.getUnknownEntity());
+        entity.setCollectionDateTime(ZonedDateTime.now());
+    }
+
+}
--- src/main/java/org/openhds/repository/generator/SampleDataGenerator.java
+++ src/main/java/org/openhds/repository/generator/SampleDataGenerator.java
@@ -3,14 +3,15 @@ package org.openhds.repository.generator;
 import org.openhds.domain.contract.AuditableCollectedEntity;
 import org.openhds.domain.contract.AuditableEntity;
 import org.openhds.domain.model.FieldWorker;
-import org.openhds.domain.model.census.*;
 import org.openhds.domain.model.update.*;
 import org.openhds.errors.model.Error;
 import org.openhds.errors.model.ErrorLog;
 import org.openhds.events.model.Event;
 import org.openhds.events.model.EventMetadata;
 import org.openhds.repository.concrete.*;
-import org.openhds.repository.concrete.census.*;
+import org.openhds.repository.concrete.census.IndividualRepository;
+import org.openhds.repository.concrete.census.LocationRepository;
+import org.openhds.repository.concrete.census.ResidencyRepository;
 import org.openhds.repository.concrete.update.*;
 import org.openhds.security.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,15 @@ public class SampleDataGenerator {
     private LocationDataGenerator locationDataGenerator;
 
     @Autowired
+    private FamilyDataGenerator familyDataGenerator;
+
+    @Autowired
+    private IndividualRepository individualRepository;
+
+    @Autowired
+    private ResidencyRepository residencyRepository;
+
+    @Autowired
     private LocationRepository locationRepository;
 
     @Autowired
@@ -41,15 +51,6 @@ public class SampleDataGenerator {
 
     @Autowired
     private FieldWorkerRepository fieldWorkerRepository;
-
-    @Autowired
-    private IndividualRepository individualRepository;
-
-    @Autowired
-    private RelationshipRepository relationshipRepository;
-
-    @Autowired
-    private ResidencyRepository residencyRepository;
 
     @Autowired
     private ErrorRepository errorRepository;
@@ -62,12 +63,6 @@ public class SampleDataGenerator {
 
     @Autowired
     private EventRepository eventRepository;
-
-    @Autowired
-    private SocialGroupRepository socialGroupRepository;
-
-    @Autowired
-    private MembershipRepository membershipRepository;
 
     @Autowired
     private VisitRepository visitRepository;
@@ -110,16 +105,7 @@ public class SampleDataGenerator {
 
         visitRepository.deleteAllInBatch();
 
-        residencyRepository.deleteAllInBatch();
-
-        membershipRepository.deleteAllInBatch();
-
-        relationshipRepository.deleteAllInBatch();
-
-        socialGroupRepository.deleteAllInBatch();
-
-        individualRepository.deleteAllInBatch();
-
+        familyDataGenerator.clearData();
         locationDataGenerator.clearData();
         requiredDataGenerator.clearData();
     }
@@ -127,33 +113,14 @@ public class SampleDataGenerator {
     public void generateSampleData() {
         requiredDataGenerator.generateData();
         locationDataGenerator.generateData(1);
+        familyDataGenerator.generateData();
 
         addVisit("visit-a", "location-1");
         addVisit("visit-b", "location-2");
         addVisit("visit-c", "location-3");
 
-        addIndividual("individual-a");
-        addIndividual("individual-b");
-        addIndividual("individual-c");
-
-        addRelationship("relationship-type-a", "individual-a", "individual-b");
-        addRelationship("relationship-type-b", "individual-c", "individual-a");
-        addRelationship("relationship-type-c", "individual-c", "individual-c");
-
-        addSocialGroup("social-group-a");
-        addSocialGroup("social-group-b");
-        addSocialGroup("social-group-c");
-
-        addMembership("memberhip-type-a", "individual-a", "social-group-a");
-        addMembership("memberhip-type-b", "individual-b", "social-group-b");
-        addMembership("memberhip-type-c", "individual-c", "social-group-c");
-
-        addResidency("resdoncy-toop-a", "individual-a", "location-1");
-        addResidency("resdoncy-toop-b", "individual-b", "location-2");
-        addResidency("resdoncy-toop-c", "individual-c", "location-3");
-
-        addDeath("death-a", "individual-a", "visit-a");
-        addDeath("death-b", "individual-b", "visit-b");
+        addDeath("death-a", "location-1-head", "visit-a");
+        addDeath("death-b", "location-2-head", "visit-b");
 
         addInMigration("migration-a");
         addInMigration("migration-b");
@@ -161,17 +128,17 @@ public class SampleDataGenerator {
         addOutMigration("migration-a");
         addOutMigration("migration-b");
 
-        addPregnancyObservation("individual-a");
+        addPregnancyObservation("location-1-head");
 
-        addPregnancyOutcome("individual-a", "individual-b", "visit-a");
+        addPregnancyOutcome("location-1-head", "location-2-head");
 
-        addPregnancyResult("birth", "individual-c");
+        addPregnancyResult("birth", "location-3-head");
 
         addPregnancyResult("stillborn", null);
 
         addErrorLog("sample error");
 
-        addEvent("sample event", "sample system");
+        addEvent("sample event");
     }
 
     private void initAuditableFields(AuditableEntity auditableEntity) {
@@ -189,18 +156,6 @@ public class SampleDataGenerator {
         auditableCollectedEntity.setCollectionDateTime(ZonedDateTime.now());
     }
 
-    private void addFieldWorker(String name, String password) {
-        FieldWorker fieldWorker = new FieldWorker();
-        initAuditableFields(fieldWorker);
-
-        fieldWorker.setFirstName(name);
-        fieldWorker.setLastName(name);
-        fieldWorker.setFieldWorkerId(name);
-        fieldWorker.setPassword(password);
-        fieldWorker.setPasswordHash(password);
-        fieldWorkerRepository.save(fieldWorker);
-    }
-
     private void addErrorLog(String description) {
         ErrorLog errorLog = new ErrorLog();
         initAuditableFields(errorLog);
@@ -214,7 +169,7 @@ public class SampleDataGenerator {
         errorLogRepository.save(errorLog);
     }
 
-    private void addEvent(String description, String system) {
+    private void addEvent(String description) {
         Event event = new Event();
         initAuditableFields(event);
 
@@ -228,68 +183,6 @@ public class SampleDataGenerator {
         event.getEventMetadata().add(defaultMetadata);
 
         eventRepository.save(event);
-    }
-
-    private void addIndividual (String name){
-        Individual individual = new Individual();
-        initAuditableFields(individual);
-        initCollectedFields(individual);
-        individual.setExtId(name);
-        individual.setFirstName(name);
-        individual.setMiddleName(name);
-        individual.setLastName(name);
-        individual.setDateOfBirth(ZonedDateTime.now().minusYears(1));
-
-        individualRepository.save(individual);
-    }
-
-    private void addSocialGroup(String name) {
-        SocialGroup socialGroup = new SocialGroup();
-        initAuditableFields(socialGroup);
-        initCollectedFields(socialGroup);
-
-        socialGroup.setExtId(name);
-        socialGroup.setGroupName(name);
-        socialGroupRepository.save(socialGroup);
-    }
-
-    private void addRelationship (String relationshipType, String individualAName, String individualBName){
-        Relationship relationship = new Relationship();
-        initAuditableFields(relationship);
-        initCollectedFields(relationship);
-        relationship.setStartDate(ZonedDateTime.now().minusYears(1));
-        relationship.setRelationshipType(relationshipType);
-        relationship.setIndividualA(individualRepository.findByExtId(individualAName).get(0));
-        relationship.setIndividualB(individualRepository.findByExtId(individualBName).get(0));
-
-        relationshipRepository.save(relationship);
-    }
-
-    private void addMembership(String type, String individualName, String socialGroupName) {
-        Membership membership = new Membership();
-        initAuditableFields(membership);
-        initCollectedFields(membership);
-
-        membership.setIndividual(individualRepository.findByExtId(individualName).get(0));
-        membership.setSocialGroup(socialGroupRepository.findByExtId(socialGroupName).get(0));
-        membership.setRelationshipToGroupHead(type);
-        membership.setStartDate(ZonedDateTime.now().minusYears(1));
-        membership.setStartType(type);
-
-        membershipRepository.save(membership);
-    }
-
-    private void addResidency(String type, String individualName, String locationName) {
-        Residency residency = new Residency();
-        initCollectedFields(residency);
-        initAuditableFields(residency);
-
-        residency.setIndividual(individualRepository.findByExtId(individualName).get(0));
-        residency.setLocation(locationRepository.findByExtId(locationName).get(0));
-        residency.setStartType(type);
-        residency.setStartDate(ZonedDateTime.now().minusYears(1));
-
-        residencyRepository.save(residency);
     }
 
     private void addVisit(String name, String locationName) {
@@ -362,7 +255,7 @@ public class SampleDataGenerator {
         pregnancyObservationRepository.save(pregnancyObservation);
     }
 
-    private void addPregnancyOutcome(String motherName, String fatherName, String visitName){
+    private void addPregnancyOutcome(String motherName, String fatherName){
         PregnancyOutcome pregnancyOutcome = new PregnancyOutcome();
         initAuditableFields(pregnancyOutcome);
         initCollectedFields(pregnancyOutcome);
