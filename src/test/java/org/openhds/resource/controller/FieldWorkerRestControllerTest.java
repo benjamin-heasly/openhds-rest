0f51ca404482d62af7f545903ff5fbdfe673e97a
Ben Heasly <benjamin.heasly@gmail.com>
2015-08-19 15:35:59 -0400

    test password hashing for users and fieldworkers

--- src/test/java/org/openhds/resource/controller/FieldWorkerRestControllerTest.java
+++ src/test/java/org/openhds/resource/controller/FieldWorkerRestControllerTest.java
@@ -1,20 +1,28 @@
 package org.openhds.resource.controller;
 
+import org.junit.Test;
 import org.openhds.domain.model.FieldWorker;
 import org.openhds.resource.contract.AuditableRestControllerTest;
 import org.openhds.resource.registration.FieldWorkerRegistration;
 import org.openhds.resource.registration.Registration;
 import org.openhds.service.impl.FieldWorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by Ben on 5/19/15.
  */
 public class FieldWorkerRestControllerTest extends AuditableRestControllerTest<
         FieldWorker, FieldWorkerService, FieldWorkerRestController> {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     @Override
@@ -46,6 +54,20 @@ public class FieldWorkerRestControllerTest extends AuditableRestControllerTest<
         registration.setFieldWorker(entity);
         registration.setPassword("password");
         return registration;
+    }
+
+    @Test
+    @WithUserDetails
+    public void registeredPasswordIsHashed() throws Exception {
+        FieldWorker fieldWorker = makeValidEntity("test-user", "test-id");
+        MvcResult mvcResult = this.mockMvc.perform(post(getResourceUrl())
+                .contentType(regularJson)
+                .content(toJson(makeRegistration(fieldWorker))))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        FieldWorker created = fromJson(controller.getEntityClass(), mvcResult.getResponse().getContentAsString());
+        assertTrue(passwordEncoder.matches("password", created.getPasswordHash()));
     }
 
 }
--- src/test/java/org/openhds/resource/controller/UserRestControllerTest.java
+++ src/test/java/org/openhds/resource/controller/UserRestControllerTest.java
@@ -1,20 +1,28 @@
 package org.openhds.resource.controller;
 
+import org.junit.Test;
 import org.openhds.resource.contract.UuidIdentifiableRestControllerTest;
 import org.openhds.resource.registration.Registration;
 import org.openhds.resource.registration.UserRegistration;
 import org.openhds.security.model.User;
 import org.openhds.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by Ben on 5/19/15.
  */
 public class UserRestControllerTest extends UuidIdentifiableRestControllerTest
         <User, UserService, UserRestController> {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     @Override
@@ -48,5 +56,17 @@ public class UserRestControllerTest extends UuidIdentifiableRestControllerTest
         return registration;
     }
 
+    @Test
+    @WithUserDetails
+    public void registeredPasswordIsHashed() throws Exception {
+        User user = makeValidEntity("test-user", "test-id");
+        MvcResult mvcResult = this.mockMvc.perform(post(getResourceUrl())
+                .contentType(regularJson)
+                .content(toJson(makeRegistration(user))))
+                .andExpect(status().isCreated())
+                .andReturn();
 
+        User created = fromJson(controller.getEntityClass(), mvcResult.getResponse().getContentAsString());
+        assertTrue(passwordEncoder.matches("password", created.getPasswordHash()));
+    }
 }
