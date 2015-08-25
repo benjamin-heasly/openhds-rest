d9cac31aa4f1453ab2812aebf6ec8cd5b38652cf
Ben Heasly <benjamin.heasly@gmail.com>
2015-08-24 21:18:58 -0400

    log important service and resource methods

--- src/main/java/org/openhds/logging/LoggingAspect.java
+++ src/main/java/org/openhds/logging/LoggingAspect.java
@@ -2,16 +2,19 @@ package org.openhds.logging;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 /**
  * Created by ben on 8/24/15.
  * <p>
- * Add basic logging as a cross-cutting aspect.  Add timing for service methods.
+ * Cross-cutting logging and method timing.
  *
  */
 @Component
@@ -20,38 +23,71 @@ public class LoggingAspect {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
-    @Around("execution(* org.openhds.service..find*(..))")
-    public Object logTimeMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Pointcut("execution(* org.openhds.resource..register*(..))")
+    public void resourceRegister(){}
 
+    @Pointcut("execution(* org.openhds.resource..*(..)) && @annotation(org.springframework.web.bind.annotation.RequestMapping)")
+    public void resourceRequestMapping(){}
+
+    @Pointcut("execution(* org.openhds.service..record*(..))")
+    public void serviceRecord(){}
+
+    @Pointcut("execution(* org.openhds.service..find*(..))")
+    public void serviceFind(){}
+
+    @Pointcut("execution(* org.openhds.service..create*(..)) || execution(* org.openhds.service..persist*(..))")
+    public void serviceCreate(){}
+
+    @Before("resourceRegister() || resourceRequestMapping() || serviceRecord() ")
+    public void nameAndArgs(JoinPoint joinPoint) throws Throwable {
+        StringBuilder stringBuilder = new StringBuilder();
+        printMethodName(stringBuilder, joinPoint);
+        printMethodArgs(stringBuilder, joinPoint);
+        log.info(stringBuilder.toString());
+    }
+
+    @Around("serviceFind() || serviceCreate()")
+    private Object executionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         // time the method execution
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-        Object retVal = joinPoint.proceed();
+        Object result = joinPoint.proceed();
         stopWatch.stop();
 
-        // method name
-        StringBuilder logMessage = new StringBuilder();
-        logMessage.append(joinPoint.getTarget().getClass().getName())
+        StringBuilder stringBuilder = new StringBuilder();
+        printMethodName(stringBuilder, joinPoint);
+        printMethodArgs(stringBuilder, joinPoint);
+        printResult(stringBuilder, result);
+        printExecutionTime(stringBuilder, stopWatch);
+
+        log.info(stringBuilder.toString());
+        return result;
+    }
+
+    private static void printMethodName(StringBuilder stringBuilder, JoinPoint joinPoint) {
+        stringBuilder.append(joinPoint.getTarget().getClass().getName())
                 .append(".")
                 .append(joinPoint.getSignature().getName());
+    }
 
-        // method args
-        logMessage.append("\n").append("with args:");
+    private static void printMethodArgs(StringBuilder stringBuilder, JoinPoint joinPoint) {
+        stringBuilder.append("\n").append("  with args:");
         for (Object object : joinPoint.getArgs()) {
-            logMessage.append("\n").append(object);
+            stringBuilder.append("\n    ")
+                    .append(object);
         }
+    }
 
-        // method return
-        logMessage.append("\n").append("with result:");
-        logMessage.append("\n").append(retVal);
+    private static void printResult(StringBuilder stringBuilder, Object result) {
+        stringBuilder.append("\n").append("  with result:");
+        stringBuilder.append("\n    ")
+                .append(result);
+    }
 
-        // method execution time
-        logMessage.append("\n").append("took ")
+    private static void printExecutionTime(StringBuilder stringBuilder, StopWatch stopWatch) {
+        stringBuilder.append("\n").append("  execution took ")
                 .append(stopWatch.getTotalTimeMillis())
                 .append(" ms");
-
-        log.info(logMessage.toString());
-        return retVal;
     }
 
 }
