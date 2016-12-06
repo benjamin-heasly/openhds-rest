0a0d24ca63ec3e949bde93ca3a92c2b6fcf1ac19
john.downs <john.downs@yodle.com>
2016-12-06 17:30:49 -0500

    extid generator stub class

---
+++ src/main/java/org/openhds/domain/util/ExtIdGenerator.java
@@ -0,0 +1,29 @@
+package org.openhds.domain.util;
+
+public class ExtIdGenerator {
+
+    public String suggestNextId(String type) {
+        //individual
+        //location
+        //location hierarchy
+        //social group
+        //visit
+
+        switch(type) {
+        case: "Individual":
+            break;
+        case "Location":
+            break;
+        case "LocationHierarchy":
+            break;
+        case: "SocialGroup":
+            break;
+        case: "Visit":
+            break;
+        default:
+            break;
+        }
+
+        return "";
+    }
+}
